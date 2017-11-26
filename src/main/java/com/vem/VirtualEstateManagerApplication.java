15ab4f9704457cdd07f8917f562fd37b49653c82
Alex Kouthoofd <akouthoo@oswego.edu>
2017-11-25 21:40:00 -0500

    Redo Spring MVC project

--- src/main/java/com/vem/VirtualEstateManagerApplication.java
+++ src/main/java/com/vem/VirtualEstateManagerApplication.java
@@ -3,10 +3,13 @@ package com.vem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by alex on 11/25/2017.
+ */
 @SpringBootApplication
 public class VirtualEstateManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VirtualEstateManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualEstateManagerApplication.class, args);
+    }
 }
