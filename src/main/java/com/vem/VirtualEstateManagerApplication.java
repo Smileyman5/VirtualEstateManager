5ca9aa2e183b3b6b212833fb11b43c8a74537087
Alex Kouthoofd <akouthoo@oswego.edu>
2017-11-25 20:01:46 -0500

    Initial commit

---
+++ src/main/java/com/vem/VirtualEstateManagerApplication.java
@@ -0,0 +1,12 @@
+package com.vem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualEstateManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualEstateManagerApplication.class, args);
+	}
+}
